503f2225d35a86d8d046328158371838546c11d7
Wynn Miller <wynnmiller@protonmail.com>
2022-12-02 10:34:35 -0500

    first commit of the day

---
+++ controller.py
@@ -0,0 +1,9 @@
+class Controller():
+    def __init__(self):
+        self.a = 0b10000000
+        self.b = 0b01000000
+        self.select = 0b00100000
+        self.start = 0b00010000
+        self.up = 0b00001000
+        self.down = 0b00000100
+        self.left = 0b00000010
--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from cynes import NES
+from cynes import * 
 import os
+import time
+import random
 
 nes = NES("roms/tetris.nes")
 
 def initialize():
     nes.reset()
-    nes.step()
+    started = False
+    while not started:
+        print(nes[0x00C3])
+        if nes[0x00C3] == 0:
+            for i in range(4):
+                nes.controller = NES_INPUT_START
+                nes.step(frames=20)
+                nes.controller = 0
+            started = True
+        nes.step()
+
+initialize()
+
 
 while not nes.should_close():
+    nes.controller = 0
     # Inputs for neural network
     piece_x = nes[0x0040]
     piece_y = nes[0x0041]
     piece_id = nes[0x0042]
-
+    current_speed = nes[0x0044]
 
     board = [
             [nes[0x0400 + i] for i in range(10)],
@@ -42,6 +57,7 @@ while not nes.should_close():
     frame = nes[0x00B2] << 8 | nes[0x00B1]
 
     if frame % 100 == 0:
+        print("Frame: ", frame)
         os.system("clear")
         for i in range(20):
             for j in range(10):
@@ -50,6 +66,11 @@ while not nes.should_close():
                 else:
                     print("[ ]", end="")
             print()
+        print("Controller Initial:", format(nes[0x00B5], '08b'))
+        print("Controller:", format(nes[0x00B6], '08b'))
+        print("Seed:", seed)
         print(piece_x, piece_y, piece_id)
+
+
 
     frame = nes.step()
