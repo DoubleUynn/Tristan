38159d13d0b48c9c9fa1c1814369659f7f0642be
Wynn Miller <wynnmiller@protonmail.com>
2025-06-02 12:41:02 -0400

    final changes to headless_train, modified config

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ def suppress_ctrl_c():
     signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
 
 MINDS_DIR = 'minds'
-POPULATION_SIZE = 50 
-PARENTS_SIZE = 2
-MUTATION_RATE = 75 
+POPULATION_SIZE = 100
+PARENTS_SIZE = 8
+MUTATION_RATE = 100
 MUTATION_FREQUENCY = 70
 CROSSING_PROBABILITY = 30
 EPOCHS = 500
