fixed softmax activation function

genetic_algorithm.py
@@ -134,7 +134,7 @@ class Brain(nn.Module):
                                  nn.Linear(self.hidden_nodes6, self.hidden_nodes7),
                                  nn.ReLU(),
                                  nn.Linear(self.hidden_nodes7, self.out_nodes),
-                                 nn.Softmax())
+                                 nn.Softmax(dim=-1))
 
     def activate(self, inputs):
         # Get the next move from the network

headless_train.py
@@ -108,8 +108,9 @@ def run(mind_num, initializer):
         # Run neural network
         if actable:
             outputs = brain.activate(inputs)
-            action = np.random.choice(len(outputs), p=outputs)
-            #action = outputs.index(max(outputs))
+            probs = np.array(outputs)
+            probs = probs / probs.sum()
+            action = np.random.choice(len(outputs), p=probs)
             nes.controller = actions[action]
             last_action = actions[action]
             last_board = board